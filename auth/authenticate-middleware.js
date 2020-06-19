dda4012072da510f8c85574f8e48d2bc59697a7e
emilio <ediaz-goico@me.bergen.edu>
2020-06-19 13:51:44 -0400

    finished mvp, last sprint baby

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken')
+const jwtKey = require('../config/constants').jwt_key
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, jwtKey, (error, decodeToken) => {
+      if (error) {
+        res.status(401).json({ message: 'INVALID TOKEN' })
+      }
+      else {
+        req.decodeToken
+        next()
+      }
+    })
+  }
+  else {
+    res.status(401).json({ you: 'YOU SHALL NOT PASS!' });
+  }
 };
